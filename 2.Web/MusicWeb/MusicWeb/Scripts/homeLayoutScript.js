e6a157533e6626dbcc448741a7042c155187da74
nttaif <nt.tai5304@gmail.com>
2023-10-06 10:40:37 +0700

    Hoanf thiện tiến trình được 30% Xây dựng login, signup Xây dựng 50% home

--- 2.Web/MusicWeb/MusicWeb/Scripts/homeLayoutScript.js
+++ 2.Web/MusicWeb/MusicWeb/Scripts/homeLayoutScript.js
@@ -1,1 +1,24 @@
-﻿
+﻿/*xử lí sự kiện dừng và phát nhạc*/
+var playBtn = document.getElementById("play");
+var audio = document.getElementById("audio-control");
+var pauseBtn = document.getElementById("stop")
+var checkPlay;
+var playAudio = function () {
+    audio.play();
+    checkPlay = true;
+};
+var pauseAudio = function () {
+    audio.pause();
+    checkPlay = false;
+}
+playBtn.addEventListener("click", playAudio, false);
+pauseBtn.addEventListener("click", pauseAudio, false)
+/*xử lí sự kiện kéo thả thanh control-time và currentTime durranTime*/
+/*let time = document.getElementById("playback-position");
+let curr=audio.currentTime
+audio.ontimeupdate = function () { myFunction() };
+audio.ontimeupdate = function () {myFunction()}
+function myFunction()
+{
+    time.innerHTML(curr);
+}*/
